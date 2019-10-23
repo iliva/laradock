6606056fc3271e7266ff4a7a1af33aad99890258
Artem Zakharchenko <artz.relax@gmail.com>
2019-10-23 14:23:59 +0300

    Orion test db

--- mariadb/docker-entrypoint-initdb.d/createdb.sql
+++ mariadb/docker-entrypoint-initdb.d/createdb.sql
@@ -7,4 +7,7 @@ GRANT ALL ON `orion`.* TO 'homestead'@'%';
 CREATE DATABASE IF NOT EXISTS `gvv_test` COLLATE 'utf8_general_ci';
 GRANT ALL ON `gvv_test`.* TO 'homestead'@'%';
 
+CREATE DATABASE IF NOT EXISTS `orion_test` COLLATE 'utf8_general_ci';
+GRANT ALL ON `orion_test`.* TO 'homestead'@'%';
+
 FLUSH PRIVILEGES;
