dbfeb8f50a7841a620fe4ba7fc1479cda98655b7
shoxy <shoxyoyo@gmail.com>
2020-03-08 22:31:00 +0200

    wip

--- mariadb/docker-entrypoint-initdb.d/createdb.sql
+++ mariadb/docker-entrypoint-initdb.d/createdb.sql
@@ -1,13 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `gvv` COLLATE 'utf8_general_ci';
-GRANT ALL ON `gvv`.* TO 'homestead'@'%';
-
-CREATE DATABASE IF NOT EXISTS `orion` COLLATE 'utf8_general_ci';
-GRANT ALL ON `orion`.* TO 'homestead'@'%';
-
-CREATE DATABASE IF NOT EXISTS `gvv_test` COLLATE 'utf8_general_ci';
-GRANT ALL ON `gvv_test`.* TO 'homestead'@'%';
-
-CREATE DATABASE IF NOT EXISTS `orion_test` COLLATE 'utf8_general_ci';
-GRANT ALL ON `orion_test`.* TO 'homestead'@'%';
+CREATE DATABASE IF NOT EXISTS `karina` COLLATE 'utf8_general_ci';
+GRANT ALL ON `karina`.* TO 'homestead'@'%';
 
 FLUSH PRIVILEGES;
